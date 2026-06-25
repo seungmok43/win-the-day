46e26316ffd427727bc8d467550b9fe4bcacb37c
seungmok lee <seungmoklee@seungmoks-MacBook-Pro.local>
2026-06-25 16:37:47 +0900

    캐시 v3 강제 갱신

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'win-the-day-v2';
+const CACHE = 'win-the-day-v3';
 const OFFLINE = ['./index.html','./community.html','./bible-readings-2026.js','./manifest.json','./icon.svg','./logo.png'];
 
 self.addEventListener('install', e => {
